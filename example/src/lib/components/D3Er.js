66776fc80152db2ba9d881cc19126a95d42459ed
岩崎 仁是 <iwasaki@booklista.co.jp>
2021-11-06 23:56:10 +0900

    fix example

--- example/src/lib/components/D3Er.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Asshole from '@yanqirenshi/assh0le';
-
-export default function D3Er (props) {
-    const rectum = props.rectum;
-
-    return <Asshole id="d3-er-graph" rectum={rectum}/>;
-}
